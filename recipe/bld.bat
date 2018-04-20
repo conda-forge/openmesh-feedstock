3b4fe338a0a98e3d4b7a057f471871a6d44f9869
Michel Zou <xantares09@hotmail.com>
2018-04-20 14:11:18 +0200

    Update to 7.0

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,26 +1,44 @@
 
-mkdir build
-cd build
 
 set CMAKE_CONFIG="Release"
-set PYTHON_LIBRARY=%PREFIX%\libs\python%PY_VER:~0,1%%PY_VER:~2,1%.lib
+
+mkdir build && cd build
 
 cmake -LAH -G"NMake Makefiles"                               ^
   -DCMAKE_BUILD_TYPE="%CMAKE_CONFIG%"                        ^
   -DCMAKE_FIND_ROOT_PATH="%LIBRARY_PREFIX%"                  ^
   -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%"                  ^
   -DBUILD_APPS=OFF                                           ^
-  -DPYTHON_LIBRARY:FILEPATH="%PYTHON_LIBRARY%"               ^
-  -DOPENMESH_PYTHON_VERSION="%PY_VER%"                       ^
-  -DPYTHON_INSTALL_DIR="%SP_DIR%"                            ^
-  -DOPENMESH_BUILD_PYTHON_UNIT_TESTS=ON ..
+  ..
 if errorlevel 1 exit 1
 
-cmake --build . --config %CMAKE_CONFIG% --target INSTALL
+cmake --build . --config %CMAKE_CONFIG% --target install
 if errorlevel 1 exit 1
 
-move %LIBRARY_PREFIX%\lib\python\openmesh.lib %SP_DIR%
-move %LIBRARY_PREFIX%\lib\python\openmesh.pyd %SP_DIR%
+ctest --output-on-failure
+if errorlevel 1 exit 1
+
+rem set PYTHON_LIBRARY=%PREFIX%\libs\python%PY_VER:~0,1%%PY_VER:~2,1%.lib
+rem -DPYTHON_LIBRARY:FILEPATH="%PYTHON_LIBRARY%"               ^
+rem -DOPENMESH_PYTHON_VERSION="%PY_VER%"                       ^
+rem -DOPENMESH_BUILD_PYTHON_UNIT_TESTS=ON ..
+
+git clone https://www.graphics.rwth-aachen.de:9000/OpenMesh/openmesh-python.git
+cd openmesh-python
+git checkout 1.1.0
+mkdir build && cd build
+cmake -LAH -G"NMake Makefiles"                               ^
+  -DCMAKE_BUILD_TYPE="%CMAKE_CONFIG%"                        ^
+  -DCMAKE_FIND_ROOT_PATH="%LIBRARY_PREFIX%"                  ^
+  -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%"                  ^
+  ..
+if errorlevel 1 exit 1
+
+cmake --build . --config %CMAKE_CONFIG% --target install
+if errorlevel 1 exit 1
+
+rem move %LIBRARY_PREFIX%\lib\python\openmesh.lib %SP_DIR%
+rem move %LIBRARY_PREFIX%\lib\python\openmesh.pyd %SP_DIR%
 
 ctest --output-on-failure
 if errorlevel 1 exit 1
--- recipe/build.sh
+++ recipe/build.sh
@@ -1,17 +1,30 @@
 #!/bin/sh
 
-mkdir -p build && cd build
+mkdir build && cd build
 
 cmake \
   -DCMAKE_FIND_ROOT_PATH=${PREFIX} \
   -DCMAKE_INSTALL_PREFIX=${PREFIX} \
   -DBUILD_APPS=OFF \
-  -DOPENMESH_PYTHON_VERSION=${PY_VER} \
-  -DOPENMESH_BUILD_PYTHON_UNIT_TESTS=ON \
   ..
 
 make install -j${CPU_COUNT}
 mv ${PREFIX}/lib/python/* ${SP_DIR}
 
 # osx, py34: ***Exception: SegFault in test_load_obj_with_material (test_read_write_obj.ReadWriteOBJ)
+ctest --output-on-failure || echo "failed"
+
+git clone https://www.graphics.rwth-aachen.de:9000/OpenMesh/openmesh-python.git
+cd openmesh-python
+git checkout 1.1.0
+
+mkdir build && cd build
+
+#   -DOPENMESH_PYTHON_VERSION=${PY_VER} \
+#   -DOPENMESH_BUILD_PYTHON_UNIT_TESTS=ON \
+cmake \
+  -DCMAKE_FIND_ROOT_PATH=${PREFIX} \
+  -DCMAKE_INSTALL_PREFIX=${PREFIX} \
+  ..
+
 ctest --output-on-failure || echo "failed"
